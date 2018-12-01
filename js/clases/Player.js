0b803296589f3831d653fdec82efcdab1d71bc4d
Bertum <albertodelcru@gmail.com>
2018-12-01 14:01:57 +0100

    Funcion preparada para la rotacion

--- js/clases/Player.js
+++ js/clases/Player.js
@@ -8,7 +8,14 @@ function Player(x, y, angulo, velocidad) {
     this.spritePosition = 0;
 
     this.drawPlayer = function () {
+        //Set the origin to the center of the image
+        //contextoPlayer.translate(this.posX + 35 / 2, this.posY + 55 / 2);
+        //Rotate the canvas around the origin
+        //contextoPlayer.rotate(this.angulo - Math.PI / 2);
         contextoPlayer.drawImage(pjSprite, this.spritePosition * 35, 0, 35, 55, this.posX, this.posY, 35, 55)
+        //reset the canvas  
+        //contextoPlayer.rotate((this.angulo - Math.PI / 2) * (-1));
+        //contextoPlayer.translate((this.posX + 35 / 2) * (-1), (this.posY + 55 / 2) * (-1));
         this.spritePosition++;
         if (this.spritePosition == 6) {
             this.spritePosition = 0;
